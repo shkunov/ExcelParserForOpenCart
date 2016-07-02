5be263e2e18c6a2c8c7739ad574596eb13fa1c9d
Mikhail <retverial@mail.ru>
2016-07-02 13:50:19 +0400

    Add methods and enums

--- UI/EnumPrices.cs
+++ UI/EnumPrices.cs
@@ -9,6 +9,12 @@ namespace ExcelParserForOpenCart
     public enum EnumPrices
     {
         ДваСоюза = 0,
-        OJ = 1
+        OJ = 1,
+        tdgroup = 2,
+        lapter = 3,
+        composite = 4,
+        rival = 5,
+        ptgroup = 6,
+        pyanov = 7,
     }
 }
