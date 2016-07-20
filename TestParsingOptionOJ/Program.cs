68a895cc92df1ff6ea6cb130600ebd77fcd342e7
kozintsev <o.kozintsev@gmail.com>
2016-07-20 21:59:15 +0400

    new algorithm

--- TestParsingOptionOJ/Program.cs
+++ TestParsingOptionOJ/Program.cs
@@ -15,44 +15,20 @@ namespace TestParsingOptionOJ
 
             foreach (var s in test)
             {
-                var input = s.Replace("опция", string.Empty).Trim();
+                string input;
+                if (s.Contains("(опция"))
+                {
+                    input = s.Replace("опция", string.Empty)
+                        .Replace(")", string.Empty)
+                        .Replace("(", ";")
+                        .Replace(",", string.Empty);
+                    Console.WriteLine(input);
+                    continue;
+                }
+                input = s.Replace("опция", string.Empty).Trim();
                 if (input[0] == '(') input = input.Replace("(", string.Empty);
                 input = input.Replace(",", ";").Replace("(", ";").Replace(")", string.Empty);
                 Console.WriteLine(input);
-            }
-
-            //foreach (var s in test)
-            //{
-            //    if (s.Contains("(опция"))
-            //    {
-                    
-            //    }
-            //    var input = s.Replace("опция", string.Empty).Trim();
-            //    if (input[0] == '(') input = input.Replace("(", string.Empty);
-            //    input = input.Replace(",", ";").Replace("(", ";").Replace(")", string.Empty);
-            //    Console.WriteLine(input);
-            //}
-
-            foreach (var s in test)
-            {
-                var input = s.Replace("опция", string.Empty).Trim();
-                var pattern = "((\\w?стандарт)|(\\w+\\s[0-9-]+))";
-                var regex = new Regex(pattern);
-
-                // Получаем совпадения в экземпляре класса Match
-                var match = regex.Match(input);
-
-                // отображаем все совпадения
-                while (match.Success)
-                {
-                    // Т.к. мы выделили в шаблоне одну группу (одни круглые скобки),
-                    // ссылаемся на найденное значение через свойство Groups класса Match
-                    var str = match.Groups[1].Value;
-                    Console.WriteLine(match.Groups[1].Value);
-
-                    // Переходим к следующему совпадению
-                    match = match.NextMatch();
-                }
             }
 
             Console.ReadLine();
