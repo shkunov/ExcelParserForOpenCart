3bad32effd0c1c51fa3275c12f5f675e3e3f5496
kozintsev <o.kozintsev@gmail.com>
2016-08-08 21:24:09 +0400

    add Lewandowski parsing algoripthm

--- UI/Prices/Lewandowski.cs
+++ UI/Prices/Lewandowski.cs
@@ -13,7 +13,42 @@ namespace ExcelParserForOpenCart.Prices
 
         public void Analyze(int row, Range range)
         {
-            
+            if (Worker.CancellationPending)
+            {
+                E.Cancel = true;
+                return;
+            }
+            var startTable = false;
+            ResultingPrice.Clear();
+            for (var i = 7; i < row; i++)
+            {
+                if (Worker.CancellationPending)
+                {
+                    E.Cancel = true;
+                    ResultingPrice.Clear();
+                    break;
+                }
+                var line = new OutputPriceLine();
+                var str = ConverterToString(range.Cells[i, 1] as Range);
+                if (string.IsNullOrWhiteSpace(str.Trim())) startTable = false;
+                var category1 = ConverterToString(range.Cells[i, 2] as Range);
+                if (!startTable) line.Category1 = category1;
+                if (str.Contains("Наименование"))
+                {
+                    startTable = true;
+                    continue;
+                }
+                // todo: нужно вычислить артикуль
+                //line.VendorCode = ;
+                if (startTable)
+                {
+                    line.Name = ConverterToString(range.Cells[i, 2] as Range);
+                    line.Cost = ConverterToString(range.Cells[i, 4] as Range);
+                    if (!string.IsNullOrEmpty(line.Name))
+                        ResultingPrice.Add(line);
+                }
+                if (string.IsNullOrWhiteSpace(category1)) break;
+            }
         }
 
     }
