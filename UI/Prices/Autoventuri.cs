8a548588def3e978617b0430f775d029894b4d70
kozintsev <o.kozintsev@gmail.com>
2016-08-16 11:58:09 +0400

    refactoring

--- UI/Prices/Autoventuri.cs
+++ UI/Prices/Autoventuri.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using System.ComponentModel;
+﻿using System.ComponentModel;
 using Microsoft.Office.Interop.Excel;
 
 namespace ExcelParserForOpenCart.Prices
@@ -41,13 +40,13 @@ namespace ExcelParserForOpenCart.Prices
                     var sc = color.ToString();
                     if (sc == "11842740") // 1 категория
                     {
-                        category1 = str.TrimStart(new Char[] {' '});
+                        category1 = str.TrimStart(' ');
                         category2 = string.Empty;
                         continue;
                     }
                     if (sc == "12829635") // 2 категория
                     {
-                        category2 = str.TrimStart(new Char[] { ' ' });
+                        category2 = str.TrimStart(' ');
                         continue;
                     }
                 }
@@ -58,7 +57,7 @@ namespace ExcelParserForOpenCart.Prices
                     Category2 = category2
                 };
                 var vendorCode = ConverterToString(range.Cells[i, 3] as Range);
-                line.Name = ConverterToString(range.Cells[i, 2] as Range).TrimStart(new Char[] { ' ' });//тримим пробелы вначале строки
+                line.Name = ConverterToString(range.Cells[i, 2] as Range).TrimStart(' ');//тримим пробелы вначале строки
                 if (string.IsNullOrEmpty(vendorCode) && !string.IsNullOrEmpty(line.Name))
                     continue; // игнорировать строки без артикля
                 line.Cost = ConverterToString(range.Cells[i, 6] as Range);
